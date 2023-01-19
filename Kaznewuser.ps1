1a7ecc479caf00e183fe9e54c664a551129e774a
unknown <Carolina@example.com>
2023-01-19 20:40:00 +0000

    New users for blackiness

--- Kaznewuser.ps1
+++ Kaznewuser.ps1
@@ -1,11 +1,11 @@
 $password = @{
-    Password = 'Githioro2007'
+    Password = 'password1234'
 }
 
 
-New-MgUser -DisplayName 'Caroline Mbagaya'`
-            -PasswordProfile $passwordprofile `
-            -AccountEnabled`
-            -MailNickname 'CarolinaM'`
-            -UserPrincipal 'CarolinaM@blanknessplc.onmicrosoft.com'`
+New-MgUser -DisplayName 'Caroline Mbagaya' `
+            -PasswordProfile $password `
+            -AccountEnabled `
+            -MailNickname 'CarolinaM' `
+            -UserPrincipal 'CarolinaM@blanknessplc.onmicrosoft.com' `
             -UsageLocation GB
